42e2acf8c37a1ceea2c261f2b44d6cc581dc77c6
Ray-Shubham <raysubham555@gmail.com>
2025-03-17 14:42:47 +0530

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import BlurBackground from "./components/BlurBackground";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
     <>
       <BlurBackground />
       <Navbar />
+      <main className="antialiased overflow-x-hidden max-w-7xl mx-auto relative z-10">
+        <Navbar />
+        <Hero />
+        <Projects />
+      </main>
     </>
   );
 };
