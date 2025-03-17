9634f7d2892d936a8bbdaa668b99d63e5dd8d46c
Ray-Shubham <raysubham555@gmail.com>
2025-03-18 00:55:44 +0530

    About Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import BlurBackground from "./components/BlurBackground";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import About from "./components/About";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <Projects />
+        <About />
       </main>
     </>
   );
